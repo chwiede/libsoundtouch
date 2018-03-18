016cb5cfa5b791bdaaa7590fdcc02d7262f2e3d2
Christoph Wiedemann <chwiede@gmail.com>
2018-03-18 12:52:15 +0100

    Issue #27: Calling browser.cancel()
    Allow zeroconf as external instance

--- libsoundtouch/__init__.py
+++ libsoundtouch/__init__.py
@@ -24,10 +24,11 @@ def soundtouch_device(host, port=8090):
     return s_device
 
 
-def discover_devices(timeout=5):
+def discover_devices(timeout=5, zeroconf=None):
     """Discover devices on the local network.
 
     :param timeout: Max time to wait in seconds. Default 5
+    :param zeroconf: Global singleton zeroconf instance. Default None
     """
     devices = []
     # Using Queue as a timeout timer
@@ -38,12 +39,17 @@ def discover_devices(timeout=5):
         _LOGGER.info("%s discovered (host: %s, port: %i)", name, host, port)
         devices.append(soundtouch_device(host, port))
 
-    zeroconf = Zeroconf()
+    if zeroconf is None:
+        zeroconf = Zeroconf()
+
     listener = SoundtouchDeviceListener(add_device_function)
     _LOGGER.debug("Starting discovery...")
-    ServiceBrowser(zeroconf, "_soundtouch._tcp.local.", listener)
+    browser = ServiceBrowser(zeroconf, "_soundtouch._tcp.local.", listener)
     try:
         add_devices_queue.get(timeout=timeout)
     except Empty:
         _LOGGER.debug("End of discovery...")
+    finally:
+        browser.cancel()
+
     return devices
